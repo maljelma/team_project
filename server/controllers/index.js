7954146a7447db78bdf5e666450b75894a645a96
maljelma <133683358+maljelma@users.noreply.github.com>
2023-07-23 16:52:02 -0400

    controllers.index.js filter by date applied

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -10,11 +10,38 @@ module.exports.displayHomePage = (req, res, next) =>
         }
         else
         {
+            // fillter surveys
+            let currentSurveys = filterSurveysUsingDateOfToday(surveyList);
             res.render('index', 
             {title: 'Home',
-             SurveyList: surveyList, 
+             SurveyList: currentSurveys, 
              displayname: req.user ? req.user.displayname : ''});
         }
     });
 }
 
+let filterSurveysUsingDateOfToday = (surveyList) => 
+{
+    let selectedSurveys = [];
+    let today = new Date();
+    
+    surveyList.forEach(element => 
+    {  
+        let start = new Date(element.dateStart);
+        let end   = new Date(element.dateEnd);
+        // only if today is in  element dateStart and dateEnd range.
+        if 
+        (
+        (start.getUTCFullYear() <= today.getFullYear()) && 
+        (today.getFullYear() <= end.getUTCFullYear()) &&
+        (start.getUTCMonth() <= today.getMonth()) &&
+        (today.getMonth() <= end.getUTCMonth()) &&
+        (start.getUTCDate() <= today.getDate()) &&
+        (today.getDate() <= end.getUTCDate())
+        )
+        {
+            selectedSurveys.push(element);
+        }
+    });
+    return selectedSurveys;
+}
